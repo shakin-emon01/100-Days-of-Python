43041e6f3c8447212c9365b2a8fd965d16cea10e
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-07-29 20:45:36 +0600

    Create Data Types.py

---
+++ Data Types.py
@@ -0,0 +1,17 @@
+# Task: Print the first character of the string
+print("Hello"[0])
+
+#String
+print("123" + "456")
+
+# Integer
+print(123 + 456)
+
+#Large Integer
+print(123_456_789 + 987_654_321)
+
+# Float
+print(3.14159 + 2.71828)
+
+#Boolean
+print(True + False)
