f02be80152d3c1d85f4185fa94589464f77a1a3b
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-07-30 02:37:19 +0600

    Create Data Types Convertion.py

---
+++ Data Types Convertion.py
@@ -0,0 +1,8 @@
+# Data Types Convertion
+
+print(int("1230")+ int("1230"))
+
+UserName = input("Enter your name: ")
+Length = len(UserName)
+
+print("Number of characters in your name is: " + str(Length) )
