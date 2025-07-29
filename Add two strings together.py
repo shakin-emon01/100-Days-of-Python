7ad7a83a7a84388eddbf32a92357dc193975d6ce
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-07-29 16:41:42 +0600

    Add files via upload

---
+++ Add two strings together.py
@@ -0,0 +1,8 @@
+#This is comments
+
+#Add 2 strings using addition
+
+print("Hello " + "World")
+
+#There can't be a single space before "print" statement. Else that'll show an indentation error
+#    print("something")    X
---
+++ Print multiple times.py
@@ -0,0 +1,11 @@
+#This is comments
+
+#Print in multiple line by using "print" function several times
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+#Print in multiple line by using "print" function single time
+
+print("Hello World\nHello World\nHello World")
