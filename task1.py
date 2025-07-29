b0152ff03a43619207b60031ffb6f9108c70d9e1
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-07-29 18:23:32 +0600

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,4 @@
+print("Hello " + input("What is your name? ") + "!")
+City_Name = input("Which city are you grew up in?\n")
+Pet_Name = input("What is the name of your pet?\n")
+print("Your band name could be " + City_Name + " " + Pet_Name + ".")
