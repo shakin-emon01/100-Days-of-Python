c99801870c92c34e179eb6adac70e4a2c232aa9e
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-07-30 02:51:45 +0600

    Create Mathmatical Operations.py

---
+++ Mathmatical Operations.py
@@ -0,0 +1,9 @@
+#Mathmatical Operations
+
+print(123+456)  # Addition
+print(456-123)  # Subtraction
+print(123*456)  # Multiplication
+print(456/123)  # Division
+print(456//123) # Floor Division convert to integer
+print(123%456)  # Modulus
+print(2**4) # Exponentiation
