f6083a094589262d150ebcac841f8ef9b10f384f
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-07-30 23:54:45 +0600

    Create F-String Function.py

---
+++ F-String Function.py
@@ -0,0 +1,7 @@
+#f-strings
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is {score}, your height is {height}m, and it is {is_winning} that you are winning the game.")            
