a7ac9ad1c1fcfb485e6e9e40e0f1caca62f204fc
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-07-30 23:50:07 +0600

    Create Round function.py

---
+++ Round function.py
@@ -0,0 +1,11 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi))
+# The code calculates the Body Mass Index (BMI) using the formula: weight (kg) / height (m)^2
+
+print(round(bmi))
+# The code calculates the Body Mass Index (BMI) using the formula: weight (kg) / height (m)^2
+
+print(round(bmi, 2))
+# The code calculates the BMI and rounds it to 2 decimal places
