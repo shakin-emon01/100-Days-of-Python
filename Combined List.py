d3113a09c72e17ebae2b4f1a90feefd296450cfa
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-08-01 03:35:42 +0600

    Update Combined List.py

--- Combined List.py
+++ Combined List.py
@@ -1,4 +1,8 @@
 fruits = ["apple", "banana", "cherry", "date", "elderberry"]
 Vegetables = ["carrot", "broccoli", "spinach", "potato", "onion"]
-dirty_dozens = [fruits, Vegetables]
-print(dirty_dozens)
+dirty_dozen = [fruits, Vegetables]
+print(dirty_dozen)
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
