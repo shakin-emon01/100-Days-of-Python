002c47b5ac8b39e1b898d269d7f8753da2873d2a
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-08-01 03:31:46 +0600

    Create Combined List.py

---
+++ Combined List.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+Vegetables = ["carrot", "broccoli", "spinach", "potato", "onion"]
+dirty_dozens = [fruits, Vegetables]
+print(dirty_dozens)
