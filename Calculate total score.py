4d94fb728924eb97dd9ed75fae8c3e640d95216b
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-08-05 02:56:32 +0600

    Create Calculate total score.py

---
+++ Calculate total score.py
@@ -0,0 +1,7 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 92, 65, 89]
+
+total_exam_score = sum(student_scores)
+total = 0
+for i in student_scores:
+    total += i
+print(total)
