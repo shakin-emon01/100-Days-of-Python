87c99bc9846d5487d68b367da70e7a19084cd3be
Shakin Ahammed Emon <165311768+shakin-emon01@users.noreply.github.com>
2025-08-05 03:08:47 +0600

    Create Range Functions with For Loops.py

---
+++ Range Functions with For Loops.py
@@ -0,0 +1,9 @@
+#Range Functions with For Loops
+
+for number in range(1, 11):
+  print(number)
+# The above code will print numbers from 1 to 10, inclusive.
+
+for number in range(1, 10, 2):
+    print(number)
+# The above code will print numbers from 1 to 10, inclusive, but only the odd numbers.
